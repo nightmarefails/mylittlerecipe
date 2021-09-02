bb80213bc524e92332569906bc7a2dae09e8b0f5
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2021-09-02 19:23:29 -0400

    Added basic structure of routes

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+//TODO: Import Route Files
+
+
+
+module.exports = router;
---
+++ controllers/api/user.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+//TODO: ADD GET all route '/'
+
+//TODO: ADD GET '/:id' to get user by id
+
+//TODO: ADD POST '/' to add a user
+
+//TODO ADD PUT '/:id' to update a user
+
+
+module.exports = router;
